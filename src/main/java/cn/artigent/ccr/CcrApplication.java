6c6d4b0d61df2d1df645ca0ef85a648917d7caf1
zengkai <astrand@126.com>
2019-01-19 09:08:06 +0800

    ti

---
+++ src/main/java/cn/artigent/ccr/CcrApplication.java
@@ -0,0 +1,14 @@
+package cn.artigent.ccr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcrApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/artigent/ccr/CcrApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.artigent.ccr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CcrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
